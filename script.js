36464bc9d4956f18e0833c6d4a58e109acec48d9
LuluHuman <lutfilkhaliq587@gmail.com>
2023-07-03 16:20:06 +0800

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+async function getlinks(){
+    const links = await fetch('\elemetns\links.json')
+    const data = await links.json()
+
+    const socalsele = document.getElementById("socials").children
+    const socals = Object.keys(data.socials)
+    for (let i = 0; i < socals.length; i++) {
+        socalsele[i].href = data[socalsele[i].alt.toLowerCase()]
+    }
+}
+
+getlinks()
