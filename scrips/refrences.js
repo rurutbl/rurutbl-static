7f0f6254950b5376d12b272174735e3e317370f1
LuluHuman <lutfilkhaliq587@gmail.com>
2024-03-16 13:55:20 +0800

    idc anymore

--- scrips/refrences.js
+++ scrips/refrences.js
@@ -128,7 +128,7 @@ document.loopingOn = true
 var lastRegLesson = null
 
 var _timeDifference = returnDate().getTime() - semstart.getTime();
-var weekNumber = Math.ceil(_timeDifference / millisecondsPerWeek)
+var weekNumber = 1//Math.ceil(_timeDifference / millisecondsPerWeek)
 var dbStore = new db()
 var settings = dbStore.get()
 document.listUrl = `/classes/${settings.class.level}/${settings.class.class}/${weekNumber % 2 == 0 && weekNumber > 0 ? "even" : "odd"}.json`
